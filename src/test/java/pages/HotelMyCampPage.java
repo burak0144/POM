9623876b64629042b877eca44e78cb29ee1cb681
burak <karanfilkaranfil822@gmail.com>
2022-09-27 16:45:49 +0300

    27.09.22

---
+++ src/test/java/pages/HotelMyCampPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class HotelMyCampPage {
+   public HotelMyCampPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[text()='Log in']")
+    public WebElement ilkLoginLinki;
+
+}
